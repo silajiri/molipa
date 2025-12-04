f97edf94ed69caef51bac9870cf13452d435d142
Jiří Šíla <jiri.sila@cross-ni.com>
2025-12-04 22:26:43 +0100

    feat: Final successful Firebase integration and game logic fix.

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js - Uchovává citlivé klíce oddelene od logiky
+const firebaseConfig = {
+    apiKey: "AIzaSyDglbU-Fh3jYWOZ2RsQerbZBpYl-dM8U9E", 
+    authDomain: "molipa-3921a.firebaseapp.com",
+    projectId: "molipa-3921a",
+    storageBucket: "molipa-3921a.firebasestorage.app",
+    messagingSenderId: "981161888486",
+    appId: "1:981161888486:web:c2f1e200c1fd6a92694927"
+};
